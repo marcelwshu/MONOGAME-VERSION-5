3a39e78283567826f37aab7fdd374016993a5485
marcel <marcel@PC>
2024-12-18 04:03:13 +0000

    Created Pickup class

--- MONOGAME VERSION 5/Pickup.cs
+++ MONOGAME VERSION 5/Pickup.cs
@@ -16,25 +16,15 @@ using SpriteBatch = Microsoft.Xna.Framework.Graphics.SpriteBatch;
 
 namespace MONOGAME_VERSION_5
 {
-    internal class Enemy : Debris
+    internal class Pickup : BackgroundObject
     {
 
         // Vars
-        private float DefaultSteerSpeed = 40f;
-        public float CurrentSteerSpeed;
-
-        private float DefaultMoveSpeed = 250f;
-        public float CurrentMoveSpeed;
-
-        public int Health;
-        public bool Steer; // If enemy follows player
-
 
         // Constructor
-        public Enemy(Texture2D texture, Vector2 pos, Vector2 size, int depth, int health, bool steer) : base(texture, pos, size, depth)
+        public Pickup(Texture2D texture, Vector2 pos, Vector2 size, int depth) : base(texture, pos, size, depth)
         {
-            this.Health = health;
-            this.Steer = steer;
+
         }
 
 
@@ -50,37 +40,16 @@ namespace MONOGAME_VERSION_5
             float Delta = (float)gameTime.ElapsedGameTime.TotalSeconds;
 
 
-            // Update steer & move speed
-            this.CurrentSteerSpeed = DefaultSteerSpeed + (Game1.CurrentGameSpeed / 4);
-            this.CurrentMoveSpeed = DefaultMoveSpeed + (Game1.CurrentGameSpeed / 4);
 
-
-            // Move enemy down the screen
-            pos.Y += (CurrentMoveSpeed + (Game1.CurrentGameSpeed / 4)) * Delta;
-
-
-            // Steer towards player 
-            if (Player != null && Steer)
-            {
-       
-                // Calculate direction to move towards
-                int signedDir = MathF.Sign(Player.pos.X - pos.X);
-                
-                // Rotate towards direction
-                this.rotation = MathHelper.Lerp(rotation, signedDir * -MathF.PI / 4 , 10 * Delta);
-
-                // Move x axis towards player
-                pos.X += (signedDir * CurrentSteerSpeed) * Delta;
-
-            }
+ 
 
             // Check for collisions with the player
             CheckCollisions();
         }
-        
-        public override void CheckCollisions()
+
+        public void CheckCollisions()
         {
-            base.CheckCollisions();
+            //base.CheckCollisions();
 
             // Vars
             float paddingX = 20; // Padding is used to decrease hitbox size
@@ -99,7 +68,7 @@ namespace MONOGAME_VERSION_5
                         paddedPos.Y < debris.pos.Y + debris.size.Y &&
                         paddedPos.Y + paddedSize.Y > debris.pos.Y)
                     {
-   
+
                         // Collision detected, delete debri
                         Game1._sceneManager.activeSprites.Remove(debris);
 
